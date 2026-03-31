e6ec1f8dde2b5f6253f00f9de09087002c4f175f
bobprince <bobprince87@gmail.com>
2026-03-31 12:49:11 +0100

    feat: update next.js config

--- next.config.ts
+++ next.config.ts
@@ -10,16 +10,14 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' 'unsafe-eval';
-    connect-src 'self' https://www.google-analytics.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' data: https:;
-    font-src 'self' https:;
-    connect-src 'self' ws: wss:;
-    frame-src 'self' https://www.google.com https://www.google.com/maps;
-
-  `.replace(/\n/g, " "),
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              connect-src 'self' https://www.google-analytics.com https://api.accian.co.uk ws: wss:;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+              font-src 'self' https:;
+              frame-src 'self' https://www.google.com https://www.google.com/maps;
+            `.replace(/\n/g, " "),
           },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "X-Frame-Options", value: "DENY" },
